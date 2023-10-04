8429638abd8acc77fdfd5cf9783dc9f16d9b538c
lize.live <git@lize.live>
2023-10-03 23:20:26 -0700

    working on flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,36 @@
 {
-  description = "A very basic flake";
+  description = "Description for the project";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs.game = {
+      type = "github";
+      owner = "minetest";
+      repo = "minetest_game";
+      flake = false;
   };
+
+  outputs = inputs@{ nixpkgs, flake-parts, game, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+        packages.default = pkgs.minetestserver;
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+        outputs.game = game;
+      };
+    };
 }
